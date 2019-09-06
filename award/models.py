5393f7fbb02e6efd026b350c7ae7f632d4a27f09
vincentouma <vinceoumah@gmail.com>
2019-09-06 17:07:28 +0300

    Create project model

--- award/models.py
+++ award/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator,MaxValueValidator
 
 # Create your models here.
+
+class Projects(models.Model):
+    name = models.CharField(max_length =30,null=True)
+    screenshot = models.ImageField(upload_to = 'images/',null=True)
+    description = models.TextField(null=True)
+    link = models.URLField()
+    user = models.ForeignKey(User, null=True)
+
+    def __str__(self):
+        return self.name
+
+    def save_project(self):
+        self.save()
+
+    def delete_project(self):
+        self.delete()
+
+    class Meta:
+        ordering = ["-id"]
+
+    @classmethod
+    def get_projects(cls):
+        projects = cls.objects.all()
+        return projects
