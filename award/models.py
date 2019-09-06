da9fb3a9da60b9cc5c1d811865a87a3f00836ab8
vincentouma <vinceoumah@gmail.com>
2019-09-06 17:46:08 +0300

    Create profile model

--- award/models.py
+++ award/models.py
@@ -27,3 +27,32 @@ class Projects(models.Model):
     def get_projects(cls):
         projects = cls.objects.all()
         return projects
+
+
+class Profile(models.Model):
+    name = models.CharField(max_length =30,null=True)
+    profile_photo = models.ImageField(upload_to = 'pics/')
+    bio = models.TextField(max_length=100,blank=True)
+    user_id = models.OneToOneField(User,on_delete=models.CASCADE)
+
+    def save_profile(self):
+        self.save()
+
+    def delete_profile(self):
+        self.delete()
+
+    def save_user(self):
+        self.save()
+
+    @classmethod
+    def search_user(cls,username):
+        searched_user = User.objects.get(username = username)
+        return searched_user
+
+    @classmethod
+    def get_profiles(cls):
+        profiles = cls.objects.all()
+        return profiles
+
+    def __str__(self):
+        return self.user.username
