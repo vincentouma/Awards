d2247a3c9a4a9a155ba7f139885d933887bc3840
vincentouma <vinceoumah@gmail.com>
2019-09-06 16:55:14 +0300

    create home view function

--- award/views.py
+++ award/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User
+from django.http  import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404
+from .forms import  *
+from .models import *
 # Create your views here.
+
+def home(request):
+   
+    return render(request, 'home.html',
+    )
