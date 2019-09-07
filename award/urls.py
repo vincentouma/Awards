713530ebbd59cd6f15afecbf67ff013194d77091
vincentouma <vinceoumah@gmail.com>
2019-09-07 15:36:41 +0300

    create search url

--- award/urls.py
+++ award/urls.py
@@ -9,5 +9,7 @@ urlpatterns = [
     url(r'^new_projects/$', views.new_projects, name='new_projects'),
     url(r'^projects/(\d+)',views.projects,name='projects'),
     url(r'profile',views.profile, name='profile'),
+    url(r'^search/', views.search_results, name='search_results'),
+
 
 ]
