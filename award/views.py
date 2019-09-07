cac6b95164c49513e75447b55355eaa3055d5037
vincentouma <vinceoumah@gmail.com>
2019-09-07 15:37:34 +0300

    create a search funtion

--- award/views.py
+++ award/views.py
@@ -94,3 +94,19 @@ def projects(request,id):
                     rating.save()
                     return redirect('home')
     return render(request,'projects.html',{"form":form, "design":design, "creativity":creativity, "content":content, "design":design, "usability":usability, "post":post})
+
+
+
+@login_required(login_url='/accounts/login/')
+def search_results(request):
+    if 'username' in request.GET and request.GET["username"]:
+        search_term = request.GET.get("username")
+        searched_users = User.objects.filter(username=search_term)
+        message = f"{search_term}"
+        profiles=  Profile.objects.all( )
+
+        return render(request, 'search.html',{"message":message,"users": searched_users,'profiles':profiles})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'all-posts/search.html',{"message":message})    
