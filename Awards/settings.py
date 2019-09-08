f18276618c8d6f4dddd5166ff502a0c856651e0c
vincentouma <vinceoumah@gmail.com>
2019-09-08 10:58:05 +0300

    deployment

--- Awards/settings.py
+++ Awards/settings.py
@@ -108,14 +108,14 @@ WSGI_APPLICATION = 'Awards.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'zawadi',
-        'USER': 'moringa',
-    'PASSWORD':'vinceobindi1005',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'zawadi',
+#         'USER': 'moringa',
+#     'PASSWORD':'vinceobindi1005',
+#     }
+# }
 
 
 # Password validation
