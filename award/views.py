599468670bcfdc26ebb0e3504e3b316f56099079
vincentouma <vinceoumah@gmail.com>
2019-09-09 10:39:23 +0300

    add profile view function

--- award/views.py
+++ award/views.py
@@ -6,6 +6,7 @@ from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404
 from tinymce.models import HTMLField
+from django.core.exceptions import ObjectDoesNotExist
 from .forms import  *
 from .models import *
 # Create your views here.
@@ -16,19 +17,45 @@ def home(request):
    return render(request,'home.html',{'projects':projects})
     
 @login_required(login_url='/accounts/login/')
-def profile(request):
+def new_profile(request):
+    current_user = request.user
     if request.method == 'POST':
-        form = ProjectForm(request.POST,request.FILES)
+        form = ProfileForm(request.POST, request.FILES)
+        if form.is_valid():
+            profile = form.save(commit=False)
+            profile.prof_user = current_user
+            profile.profile_Id = request.user.id
+            profile.save()
+        return redirect('profile')
+    else:
+        form = ProfileForm()
+    return render(request, 'profile/new_profile.html', {"form": form})
 
+@login_required(login_url='/accounts/login/')
+def profile_edit(request):
+    current_user = request.user
+    if request.method == 'POST':
+        logged_user = Profile.objects.get(prof_user=request.user)
+        form = ProfileForm(request.POST, request.FILES, instance=logged_user)
         if form.is_valid():
             form.save()
-            return redirect('profile')
-            return render(request, 'profile.html')
+        return redirect('profile')
     else:
-        form = ProjectsForm()
+        form = ProfileForm()
+    return render(request,'profile/edit_profile.html',{'form':form})
 
-        my_profile = Profile.objects.all()
-    return render(request,'profile.html', locals())    
+@login_required(login_url='/accounts/login/')
+def profile(request):
+    current_user = request.user
+    projects = Projects.objects.filter(user = current_user)
+
+    try:   
+        prof = Profile.objects.get(prof_user=current_user)
+    except ObjectDoesNotExist:
+        return redirect('new_profile')
+
+    return render(request,'profile/profile.html',{'profile':prof,'projects':projects})
+
 
 @login_required(login_url='/accounts/login/')
 def new_projects(request):
